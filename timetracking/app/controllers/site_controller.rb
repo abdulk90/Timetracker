20a1ae9da774cd0b5702af00559e4a8bf8c55a40
abdulk90 <abdul.kayyali@gmail.com>
2016-04-18 14:15:07 -0400

    imported images into local drive

--- timetracking/app/controllers/site_controller.rb
+++ timetracking/app/controllers/site_controller.rb
@@ -7,4 +7,30 @@ class SiteController < ApplicationController
 		render 'contact'
 	end
 
+	def say_name
+
+		@name = params[:name]
+
+		render 'say_name'
+
+	end
+
+	def calculator
+	
+		render 'calculator'
+	
+	end
+
+	def calculate
+
+		@num1 = params[:first_num].to_i
+		
+		@num2 = params[:second_num].to_i
+		
+		@result = @num1 + @num2
+
+		render 'calculate'
+
+	end
+
 end
