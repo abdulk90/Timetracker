8d3a5011bc08f831a6818e6fe4367839efa1226c
abdulk90 <abdul.kayyali@gmail.com>
2016-04-19 14:05:37 -0400

    Added seeds with verious names and descriptions

--- timetracking/db/seeds.rb
+++ timetracking/db/seeds.rb
@@ -1,7 +1,21 @@
+Project.create(name:"Abdul", description:"Awesome person")
+Project.create(name:"Hazel", description:"Awesome dog")
+Project.create(name:"Lina", description:"Awesome Roomie")
+Project.create(name:"Angel", description:"Ehhh Roomie")
+Project.create(name:"Ness", description:"Earthbound")
+Project.create(name:"Lucas", description:"Mother 3")
+Project.create(name:"Pizza", description:"Favorite food")
+Project.create(name:"Hazelnut", description:"Favorite flavor coffee")
+Project.create(name:"Salads", description:"Yummy")
+Project.create(name:"Pencil", description:"Writing thing")
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
+
 # Examples:
-#
+
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
